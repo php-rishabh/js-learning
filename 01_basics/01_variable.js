3c8da658c945ad66a211006b188f10e36d1ad3f6
Rishabh Kumar Bhardwaj <123570492+php-rishabh@users.noreply.github.com>
2023-10-02 11:45:51 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1123
+var accountEmail = "email.com"
+let accountPassword = "2445"
+accountCity = "Noida"
+let accountSates;
+
+// accountId = 4554 ---const variable dose not be change 
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail = "re-email.com"
+accountPassword = "4422"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountSates])
